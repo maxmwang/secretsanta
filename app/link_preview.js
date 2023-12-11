14eaf000eaebfceda0f7e6d5f780edebee57432b
Brandon Wang <sourbrandon@gmail.com>
2023-12-10 19:24:08 -0800

    fix error

---
+++ app/link_preview.js
@@ -0,0 +1,43 @@
+const https = require('https');
+const { parse } = require('node-html-parser');
+
+async function linkPreview(url) {
+  let html = await httpget(url);
+  if (html === '') {
+    return undefined;
+  }
+  const root = parse(html);
+  const ogImage = getOgImage(root);
+  return ogImage;
+}
+
+function httpget(url) {
+  const promise = new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res.setEncoding('utf8');
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        if (res.statusCode === 200) {
+          resolve(data);
+        } else {
+          reject(data);
+        }
+      })
+    }).on('error', resolve(''));
+  });
+  return promise;
+}
+
+function getOgImage(root) {
+  const ogImage = root.querySelector('meta[property="og:image"]');
+  if (ogImage === null) {
+    return undefined;
+  }
+  return ogImage.getAttribute('content');
+}
+
+module.exports = {
+  linkPreview: linkPreview,
+}
